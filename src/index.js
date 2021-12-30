9d69beb34ae3f33ee802b6a0ba71a2ec059b3a59
Yigit Mersin <yigitmersin@gmail.com>
2021-12-30 06:01:59 +0300

    Add mobile display style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './style.css';
-
-const testFunc = () => {
-  const div = document.createElement('div');
-  div.setAttribute('style', 'font-size:3em;text-align:center;margin-top:50vh;');
-  div.innerText = 'Webpack Template';
-  document.body.append(div);
-};
-
-testFunc();
